3e0f81dfad911e8a9efff92097d2d3a602d30a9a
Tulir Asokan <tulir@maunium.net>
2018-04-27 15:14:32 +0300

    Add gomatrix version

--- jesaribot.go
+++ jesaribot.go
@@ -3,15 +3,11 @@ package main
 import (
 	"strings"
 
-	"maunium.net/go/maubot"
-	"maunium.net/go/maubot/matrix"
-	"maunium.net/go/maubot/slack"
-	"maunium.net/go/maubot/telegram"
 	"maunium.net/go/mauflag"
+	"maunium.net/go/gomatrix"
+	"encoding/json"
 )
 
-var tgToken = mauflag.MakeFull("t", "telegram", "The Telegram bot token to use", "").String()
-var slackToken = mauflag.MakeFull("s", "slack", "The Slack bot token to use", "").String()
 var matrixHomeserver = mauflag.MakeFull("m", "matrix-server", "The Matrix homeserver to use", "").String()
 var matrixUser = mauflag.MakeFull("u", "matrix-user", "The Matrix user localpart to use", "").String()
 var matrixPassword = mauflag.MakeFull("p", "matrix-password", "The Matrix password to use", "").String()
@@ -19,58 +15,53 @@ var wantHelp, _ = mauflag.MakeHelpFlag()
 
 func main() {
 	mauflag.SetHelpTitles(
-		"A simple maubot example that replies with a nice GIF when it receives the word \"jesari\" (duct tape in Finnish slang)",
-		"jesaribot [-t telegramToken] [-s slackToken] [-s matrixHomeserver] [-m matrixUser] [-p matrixPassword]")
+		"A simple gomatrix example that replies with a nice GIF when it receives the word \"jesari\" (duct tape in Finnish slang)",
+		"jesaribot [-s matrixHomeserver] [-m matrixUser] [-p matrixPassword]")
 	mauflag.Parse()
 	if *wantHelp {
 		mauflag.PrintHelp()
 		return
 	}
 
-	bot := maubot.New()
+	client, _ := gomatrix.NewClient(*matrixHomeserver, "", "")
 
-	if tgToken != nil && len(*tgToken) > 0 {
-		tg, err := telegram.New(*tgToken)
-		if err != nil {
-			panic(err)
-		}
-
-		bot.Add(tg)
-		err = tg.Connect()
-		if err != nil {
-			panic(err)
-		}
+	resp, err := client.Login(&gomatrix.ReqLogin{
+		Type:                     "m.login.password",
+		User:                     *matrixUser,
+		Password:                 *matrixPassword,
+		InitialDeviceDisplayName: "Jesaribot",
+	})
+	if err != nil {
+		panic(err)
 	}
 
-	if slackToken != nil && len(*slackToken) > 0 {
-		slck, err := slack.New(*slackToken)
-		if err != nil {
-			panic(err)
-		}
+	client.AccessToken = resp.AccessToken
+	client.UserID = resp.UserID
 
-		bot.Add(slck)
-		err = slck.Connect()
-		if err != nil {
-			panic(err)
+	syncer := client.Syncer.(*gomatrix.DefaultSyncer)
+	syncer.OnEventType("m.room.message", func(evt *gomatrix.Event) {
+		text, _ := evt.Content["body"].(string)
+		if strings.Contains(strings.ToLower(text), "jesari") {
+			client.SendMessageEvent(evt.RoomID, "m.room.message", json.RawMessage(`{
+  "body": "putkiteippi.gif",
+  "info": {
+    "mimetype": "image/gif",
+    "thumbnail_info": {
+      "mimetype": "image/png",
+      "h": 153,
+      "w": 364,
+      "size": 51302
+    },
+    "h": 153,
+    "thumbnail_url": "mxc://maunium.net/iivOnCDjcGqGvnwnNWxSbAvb",
+    "w": 364,
+    "size": 2079294
+  },
+  "msgtype": "m.image",
+  "url": "mxc://maunium.net/IkSoSYYrtaYJQeCaABSLqKiD"
+}`))
 		}
-	}
+	})
 
-	if matrixHomeserver != nil && len(*matrixHomeserver) > 0 {
-		matrixx, err := matrix.New(*matrixHomeserver, *matrixUser, *matrixPassword)
-		if err != nil {
-			panic(err)
-		}
-
-		bot.Add(matrixx)
-		err = matrixx.Connect()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	for message := range bot.Messages() {
-		if strings.Contains(strings.ToLower(message.Text()), "jesari") {
-			message.Reply("https://i.imgur.com/BftYhcU.gif")
-		}
-	}
+	client.Sync()
 }
